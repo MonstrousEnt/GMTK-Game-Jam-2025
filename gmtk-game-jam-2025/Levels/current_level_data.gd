845f1d5df7d28a29faba3e8c7695f653a42d9ee8
DangerousDaniel <danielcox996@gmail.com>
2025-07-31 22:30:06 -0300

    Update current_level_data.gd

--- gmtk-game-jam-2025/Levels/current_level_data.gd
+++ gmtk-game-jam-2025/Levels/current_level_data.gd
@@ -1,4 +1,10 @@
 extends Node
 
-# Level Ssytem Var
-@export var levelindex: int = 0
+# The index of current level
+@export var levelindex : int = 0
+
+# the level unlock or not
+@export var unlocklevel: bool = false
+
+# the level completed or not
+@export var levelCompleted : bool = false
