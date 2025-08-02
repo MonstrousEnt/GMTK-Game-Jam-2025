4cef579d754ee3d3e6a2d6792fb816b6f8ef15f1
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-08-02 10:00:33 -0300

    Added spring functionality

--- gmtk-game-jam-2025/script/PlayerController.gd
+++ gmtk-game-jam-2025/script/PlayerController.gd
@@ -1,32 +1,46 @@
+class_name Player
 extends CharacterBody2D
 
-@export var speed: float = 250
+# Variables to tweak in editor
+@export var speed: float = 350
 @export var jumpForce: float = 450
 @export var gravity: float = 980
 @export var acceleration: float = 600
-@export var deceleration: float = 2400
+@export var deceleration: float = 3000
 @export var coyote_time: float = 0.1
 @export var jump_buffer_time: float = 0.1
 @export var jump_cut_gravity_multiplier: float = 2.0
 @export var air_acceleration: float = 600 # For air control
-@export var air_deceleration: float = 2400 # For air control
+@export var air_deceleration: float = 1200 # For air control
 
+# References
 @onready var player_anim_controller = $"AnimatedSprite2D"
 @onready var input_manager = $"InputManager"
 
-var camera_rotation_state: int = 0
 var coyote_timer: float = 0.0
 var jump_buffer_timer: float = 0.0
 
+var is_sprung: bool = false
+var spring_timer: float = 0.0
+
 func _physics_process(delta) -> void:
 	apply_gravity(delta)
-	move_player(delta)
 	jump(delta)
+	
+	if is_sprung:
+		# If the player is sprung, start the timer.
+		spring_timer -= delta
+		if spring_timer <= 0:
+			is_sprung = false
+	if not is_sprung:
+		# If the spring is finished, the player can move.
+		move_player(delta)
 	move_and_slide()
 
 func apply_gravity(delta) -> void:
 	# --- GRAVITY AND TIMERS ---
 	if not is_on_floor():
+		player_anim_controller.play("jump")
 		velocity.y += gravity * delta
 		coyote_timer -= delta
 	else:
@@ -35,17 +49,13 @@ func apply_gravity(delta) -> void:
 func move_player(delta) -> void:
 	# --- HORIZONTAL MOVEMENT ---
 	var input_x = 0
-	# Check the logical actions defined in your InputManager
+	# Check the logical actions defined in the input manager
 	var move_left = input_manager.is_action_pressed("move_left")
 	var move_right = input_manager.is_action_pressed("move_right")
 	var move_up = input_manager.is_action_pressed("move_up")
 	var move_down = input_manager.is_action_pressed("move_down")
 	
-	match camera_rotation_state:
-		0: input_x = int(move_right) - int(move_left)
-		1: input_x = int(move_down) - int(move_up)
-		2: input_x = int(move_left) - int(move_right)
-		3: input_x = int(move_up) - int(move_down)
+	input_x = int(move_right) - int(move_left)
 		
 	var target_speed = input_x * speed
 	
