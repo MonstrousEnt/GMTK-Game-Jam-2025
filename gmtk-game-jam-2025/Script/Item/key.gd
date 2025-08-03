9ff09d4f6823c82adef9ece4641def73e3f75608
DangerousDaniel <danielcox996@gmail.com>
2025-08-03 09:39:23 -0300

    Key Code Clean Up

--- gmtk-game-jam-2025/Script/Item/key.gd
+++ gmtk-game-jam-2025/Script/Item/key.gd
@@ -1,4 +1,19 @@
+"""
+	Project Name: Non Euclidean Puzzle Platformer
+	Team Name: Vextor Games
+	Authors: Daniel, Kyle
+	Created Date: August 2, 2023
+	Last Updated: August 3, 2023
+	Description: This is class for keys
+	Notes: 
+	Resoucres:
+"""
+
 class_name key extends item_data
+
+##
+## Class Variables
+##
 
 # --- Resource to hold the key data (door this key unlocks) ---
 @export var door_number: int = 0
