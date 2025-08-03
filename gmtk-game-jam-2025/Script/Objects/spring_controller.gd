1f3dbb4b19462666182b2796952b3a787b8e2f3c
DangerousDaniel <danielcox996@gmail.com>
2025-08-03 09:35:00 -0300

    Spring Controller Code Clean Up

--- gmtk-game-jam-2025/Script/Objects/spring_controller.gd
+++ gmtk-game-jam-2025/Script/Objects/spring_controller.gd
@@ -1,14 +1,35 @@
-extends Area2D
+"""
+	Project Name: Non Euclidean Puzzle Platformer
+	Team Name: Vextor Games
+	Authors: Daniel, Kyle
+	Created Date: August 2, 2023
+	Last Updated: August 3, 2023
+	Description: This class is for spring objects
+	Notes: 
+	Resoucres:
+"""
 
+class_name SpringController extends Area2D
+
+##
+## Class Variables
+##
+
+#Spring
 @export var spring_power: int = 800
 @export var spring_duration: float = 0.5
 
+##
+## BUILT IN METHODS
+##
+
 #--- Function check's for collisions with player and applies spring effect. ---
 func _on_body_entered(body) -> void:
-	if body is Player:
+	if (body is Player):
 		body.player_anim_controller.stop()
 		body.is_sprung = true
 		body.spring_timer = spring_duration
+		
 		# Apply direction to the player's velocity based on spring power.
 		body.velocity.x = -cos(rotation + PI/2.0) * spring_power
 		body.velocity.y = -sin(rotation + PI/2.0) * spring_power
