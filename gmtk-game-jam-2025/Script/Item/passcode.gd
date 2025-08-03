0a9fbbdb987f166378cb056ea7c8e3d3161473e0
DangerousDaniel <danielcox996@gmail.com>
2025-08-03 09:41:27 -0300

    Passcode Code Clean Up

--- gmtk-game-jam-2025/Script/Item/passcode.gd
+++ gmtk-game-jam-2025/Script/Item/passcode.gd
@@ -1,4 +1,19 @@
-class_name passcode extends item_data
+"""
+	Project Name: Non Euclidean Puzzle Platformer
+	Team Name: Vextor Games
+	Authors: Daniel, Kyle
+	Created Date: August 2, 2023
+	Last Updated: August 3, 2023
+	Description: This is class passcodes
+	Notes: 
+	Resoucres:
+"""
+
+class_name Passcode extends item_data
+
+##
+## Class Variables
+##
 
 #--- Resource to hold the passcode data. ---
 @export var passcode: int = 1234
