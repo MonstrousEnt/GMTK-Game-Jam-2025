e797f42ee9b486c5e4fa8bfef0e59fab06dd3c2f
DangerousDaniel <danielcox996@gmail.com>
2025-08-03 09:43:47 -0300

    pickup Code Clean Up

--- gmtk-game-jam-2025/Script/Item/pickup.gd
+++ gmtk-game-jam-2025/Script/Item/pickup.gd
@@ -1,10 +1,30 @@
-extends Area2D
+"""
+	Project Name: Non Euclidean Puzzle Platformer
+	Team Name: Vextor Games
+	Authors: Daniel, Kyle
+	Created Date: August 2, 2023
+	Last Updated: August 3, 2023
+	Description: This is class for pickup items
+	Notes: 
+	Resoucres:
+"""
 
+class_name Pickup extends Area2D
+
+##
+## Class Variables
+##
+
+#Item
 @export var item_data_resource: item_data
+
+##
+## BUILT IN METHODS
+##
 
 # Check if player has entered pickup range of an item.
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
+	if (body is Player):
 		print("player is on item, so pick it up.")
 		# Add the item to the inventory.
 		InventoryManager.add_item(item_data_resource)
