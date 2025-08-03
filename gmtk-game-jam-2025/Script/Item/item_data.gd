4334964bea64acdd725228229f440f5f53dd99c1
DangerousDaniel <danielcox996@gmail.com>
2025-08-03 09:38:03 -0300

    Item Data Code Clean Up

--- gmtk-game-jam-2025/Script/Item/item_data.gd
+++ gmtk-game-jam-2025/Script/Item/item_data.gd
@@ -1,4 +1,19 @@
+"""
+	Project Name: Non Euclidean Puzzle Platformer
+	Team Name: Vextor Games
+	Authors: Daniel, Kyle
+	Created Date: August 2, 2023
+	Last Updated: August 3, 2023
+	Description: This is the data object class for item
+	Notes: 
+	Resoucres:
+"""
+
 class_name item_data extends Resource
+
+##
+## Class Variables
+##
 
 #--- Base item data class, with name and id ---
 @export var item_name: String = "item"
